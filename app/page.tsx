9a57a456447245293b3b860799d93a532e446dbd
Denis Dobroslav <dobroslav.denis@mail.ru>
2025-10-13 22:52:44 +0200

    implement homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import {Button} from "@/components/ui/button";
+import Companioncard from "@/components/ui/companioncard";
+import Companionslist from "@/components/ui/companionslist";
+import {Cta} from "@/components/ui/CTA";
 
 const Page = () => {
   return (
-    <div>
-        <h1 className="text-2xl underline">First web Saas APP</h1>
-        <Button> Lesgooooo</Button>
-    </div>
+    <main>
+        <h1 className="text-4xl">Popular companions</h1>
+        <section className={"home-section"}>
+            <Companioncard id={"11"}  name={"Neura"} topic={"Neural network"} subject={"Science"} duration={45} color={"#B0C4DE"}/>
+            <Companioncard id={"22"}  name={"Countsy"} topic={"Derivatives"} subject={"Maths"} duration={30} color={"#E6F9E6"}/>
+            <Companioncard id={"33"}  name={"Verba"} topic={"Literature"} subject={"English"} duration={30} color={"#FFFACD"}/>
+        </section>
+        <section className={"home-section"}>
+            <Companionslist/>
+            <Cta/>
+
+        </section>
+
+
+    </main>
   )
 }
 
---
+++ components/ui/companionslist.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Companionslist = () => {
+    return (
+        <div>Companionslist</div>
+    )
+}
+export default Companionslist

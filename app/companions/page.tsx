7ad289a3d2dacec8253ad906c51fcfd8651db314
Denis Dobroslav <dobroslav.denis@mail.ru>
2025-10-20 21:59:06 +0200

    Companions library implement

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import {getAllcompanions} from "@/lib/actions/companion.actions";
+import Companioncard from "@/components/ui/companioncard";
+import {getSubjectColor} from "@/lib/utils";
+import Searchinput from "@/components/ui/searchinput";
+import Subjectfilter from "@/components/ui/subjectfilter";
 
-const CompanionsLibrary = () => {
+const CompanionsLibrary  =async ({searchParams}: SearchParams) => {
+const filters=await searchParams;
+const subject = filters.subject? filters.subject:"";
+const topic = filters.topic? filters.topic:"";
+
+const companions= await getAllcompanions({subject,topic});
+
     return (
-        <div>
-
-        </div>
+        <main>
+            <section className="flext justify-between gap-4 max-sm:flex-col">
+                <h1>Companion library</h1>
+                <div className="flex gap-4">
+                    <Searchinput/>
+                    <Subjectfilter/>
+                </div>
+            </section>
+            <section className="companions-grid">
+                {companions.map((companion) => (
+                    <Companioncard key={companion.id} {...companion} color={getSubjectColor(companion.subject)} />
+                ))}
+            </section>
+        </main>
     )
 }
 
--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -12,3 +12,29 @@ const {data,error} = await supabase.from('companions').insert({...formdata,autho
 if(error || !data) throw new Error(error?.message || 'Failed to create companion');
 return data[0];
 }
+
+export const getAllcompanions = async ({limit=10, page=1,subject, topic}:GetAllCompanions) =>
+{
+    const supabase = createSupabaseclient();
+    let query= supabase.from('companions').select();
+
+    if (subject && topic){
+        query=query.ilike('subject', `%${subject}%` )
+        .or (`topic.ilike.%${topic}%,name.ilike.%${topic}%`)
+    }
+    else if(subject){
+        query=query.ilike('subject', `%${subject}%` )
+    }
+    else if(topic){
+        query=query.or (`topic.ilike.%${topic}%,name.ilike.%${topic}%`)
+    }
+
+    query=query.range((page-1) * limit, page*limit-1);
+
+    const { data:companions,error } = await query;
+
+    if (error) throw new Error(error?.message || 'Failed to show companions');
+    return companions;
+
+
+}
